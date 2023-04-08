3d5180e3b2f57e6578ac3551fd65063e739765c4
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-04-08 23:40:57 +0900

    feat: アニメーションの追加(AnimatedContainer/AnimatedSwitcher)

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,46 @@ class _MyHomePageState extends State<MyHomePage> {
       _flag = !_flag;
     });
   }
+  //AnimatedOpacity/AnimatedSize/AnimatedAlign
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           AnimatedOpacity(
+  //               opacity: _flag ? 0.1 : 1.0,
+  //               duration: const Duration(seconds: 3),
+  //               child: Text(
+  //                 "消える文字",
+  //                 style: Theme.of(context).textTheme.headline4,
+  //               )),
+  //           AnimatedSize(
+  //               duration: const Duration(seconds: 3),
+  //               child: SizedBox(
+  //                   width: _flag ? 50 : 200,
+  //                   height: _flag ? 50 : 200,
+  //                   child: Container(color: Colors.purple))),
+  //           AnimatedAlign(
+  //               duration: const Duration(seconds: 3),
+  //               alignment: _flag ? Alignment.topLeft : Alignment.bottomRight,
+  //               child: SizedBox(
+  //                   width: 50,
+  //                   height: 50,
+  //                   child: Container(color: Colors.green)))
+  //         ],
+  //       ),
+  //     ),
+  //     floatingActionButton:
+  //       FloatingActionButton(onPressed: _click, child: const Icon(Icons.add)),
+  //   );
+  // }
+
+  //AnimatedContainer/AnimatedSwitcher
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,26 +106,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            AnimatedOpacity(
-                opacity: _flag ? 0.1 : 1.0,
+            AnimatedContainer(
                 duration: const Duration(seconds: 3),
-                child: Text(
-                  "消える文字",
-                  style: Theme.of(context).textTheme.headline4,
-                )),
-            AnimatedSize(
+                width: _flag ? 100 : 50,
+                height: _flag ? 50 : 100,
+                padding: _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
+                margin: _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
+                transform: _flag ? Matrix4.skewX(0.0) : Matrix4.skewX(0.3),
+                color: _flag ? Colors.blue : Colors.grey),
+            AnimatedSwitcher(
                 duration: const Duration(seconds: 3),
-                child: SizedBox(
-                    width: _flag ? 50 : 200,
-                    height: _flag ? 50 : 200,
-                    child: Container(color: Colors.purple))),
-            AnimatedAlign(
-                duration: const Duration(seconds: 3),
-                alignment: _flag ? Alignment.topLeft : Alignment.bottomRight,
-                child: SizedBox(
-                    width: 50,
-                    height: 50,
-                    child: Container(color: Colors.green)))
+                child: _flag
+                    ? const Text("なにもない")
+                    : const Icon(Icons.favorite, color: Colors.pink))
           ],
         ),
       ),
