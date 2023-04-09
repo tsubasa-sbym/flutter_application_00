1240fc96a5772408f89f94aee63bacf3d3f5f80d
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-04-09 22:18:29 +0900

    feat: Tween/Animation追加

--- lib/main.dart
+++ lib/main.dart
@@ -49,9 +49,14 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-//AnimationControllerを使ったTransition系Widgetアニメーション
+// Tween/Animation
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   late AnimationController _animationController;
+  late Animation<double> _animationDouble;
+  final Tween<double> _tweenDouble = Tween(begin: 0.0, end: 200.0);
+  late Animation<Color?> _animationColor;
+  final ColorTween _tweenColor =
+      ColorTween(begin: Colors.green, end: Colors.blue);
 
   // 再生
   _forward() async {
@@ -60,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     });
   }
 
-  //停止
+  // 停止
   _stop() async {
     setState(() {
       _animationController.stop();
@@ -80,6 +85,16 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     super.initState();
     _animationController =
         AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    // TweenとAnimationControllerからAnimationを作る(サイズ)
+    _animationDouble = _tweenDouble.animate(_animationController);
+    _animationDouble.addListener(() {
+      setState(() {});
+    });
+    // TweenとAnimationControllerからAnimationを作る(色)
+    _animationColor = _tweenColor.animate(_animationController);
+    _animationColor.addListener(() {
+      setState(() {});
+    });
   }
 
   // 破棄
@@ -99,13 +114,16 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text("AnimationController:${_animationController.value}"),
+            Text("AnimationDouble:${_animationDouble.value}"),
+            Text("AnimationColor:${_animationColor.value}"),
             SizeTransition(
-              sizeFactor: _animationController, // AnimationControllerを設定
+              sizeFactor: _animationController,
               child: Center(
                   child: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Container(color: Colors.green))),
+                      width: _animationDouble.value,
+                      height: _animationDouble.value,
+                      child: Container(color: _animationColor.value))),
             ),
           ],
         ),
@@ -122,6 +140,80 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
     );
   }
 }
+
+//AnimationControllerを使ったTransition系Widgetアニメーション
+// class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+//   late AnimationController _animationController;
+
+//   // 再生
+//   _forward() async {
+//     setState(() {
+//       _animationController.forward();
+//     });
+//   }
+
+//   //停止
+//   _stop() async {
+//     setState(() {
+//       _animationController.stop();
+//     });
+//   }
+
+//   // 逆再生
+//   _reverse() async {
+//     setState(() {
+//       _animationController.reverse();
+//     });
+//   }
+
+//   // 生成
+//   @override
+//   void initState() {
+//     super.initState();
+//     _animationController =
+//         AnimationController(vsync: this, duration: const Duration(seconds: 3));
+//   }
+
+//   // 破棄
+//   @override
+//   void dispose() {
+//     _animationController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             SizeTransition(
+//               sizeFactor: _animationController, // AnimationControllerを設定
+//               child: Center(
+//                   child: SizedBox(
+//                       width: 50,
+//                       height: 50,
+//                       child: Container(color: Colors.green))),
+//             ),
+//           ],
+//         ),
+//       ),
+//       // 再生、停止、逆再生のボタン
+//       floatingActionButton:
+//           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+//         FloatingActionButton(
+//             onPressed: _forward, child: const Icon(Icons.arrow_forward)),
+//         FloatingActionButton(onPressed: _stop, child: const Icon(Icons.pause)),
+//         FloatingActionButton(
+//             onPressed: _reverse, child: const Icon(Icons.arrow_back)),
+//       ]),
+//     );
+//   }
+// }
 
 // class _MyHomePageState extends State<MyHomePage> {
   // bool _flag = false;
