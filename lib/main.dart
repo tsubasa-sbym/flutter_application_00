c5c9261f50bd40d649835ea1d6e88b2f6255121d
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-04-10 23:35:08 +0900

    feat: Navigatorを使ったページ遷移(mainへtest_page1への遷移を追加)

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:url_launcher/url_launcher_string.dart';
-import 'dart:math';
-import 'package:flutter/material.dart';
+import 'test_page1.dart';
+
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:url_launcher/url_launcher_string.dart';
+// import 'dart:math';
+// import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -51,76 +53,85 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// AnimationBuilder
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation _animation;
 
-  // 再生
-  _forward() async {
-    setState(() {
-      _animationController.forward();
-    });
-  }
-
-  // 停止
-  _stop() async {
-    setState(() {
-      _animationController.stop();
-    });
-  }
-
-  // 逆再生
-  _reverse() async {
-    setState(() {
-      _animationController.reverse();
-    });
-  }
-
-  // 生成
-  @override
-  void initState() {
-    super.initState();
-    _animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 1));
-    _animation = _animationController.drive(Tween(begin: 0.0, end: 2.0 * pi));
-  }
-
-  // 破棄
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
+//
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: _animation,
-          builder: (context, _) {
-            return Transform.rotate(
-                angle: _animation.value,
-                child: const Icon(Icons.cached, size: 100));
-          },
-        ),
-      ),
-      // 再生、停止、逆再生のボタン
-      floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        FloatingActionButton(
-            onPressed: _forward, child: const Icon(Icons.arrow_forward)),
-        FloatingActionButton(onPressed: _stop, child: const Icon(Icons.pause)),
-        FloatingActionButton(
-            onPressed: _reverse, child: const Icon(Icons.arrow_back)),
-      ]),
-    );
+    return Scaffold(body: TestPage1());
   }
 }
+
+// AnimationBuilder
+// class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+//   late AnimationController _animationController;
+//   late Animation _animation;
+
+//   // 再生
+//   _forward() async {
+//     setState(() {
+//       _animationController.forward();
+//     });
+//   }
+
+//   // 停止
+//   _stop() async {
+//     setState(() {
+//       _animationController.stop();
+//     });
+//   }
+
+//   // 逆再生
+//   _reverse() async {
+//     setState(() {
+//       _animationController.reverse();
+//     });
+//   }
+
+//   // 生成
+//   @override
+//   void initState() {
+//     super.initState();
+//     _animationController =
+//         AnimationController(vsync: this, duration: const Duration(seconds: 1));
+//     _animation = _animationController.drive(Tween(begin: 0.0, end: 2.0 * pi));
+//   }
+
+//   // 破棄
+//   @override
+//   void dispose() {
+//     _animationController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: AnimatedBuilder(
+//           animation: _animation,
+//           builder: (context, _) {
+//             return Transform.rotate(
+//                 angle: _animation.value,
+//                 child: const Icon(Icons.cached, size: 100));
+//           },
+//         ),
+//       ),
+//       // 再生、停止、逆再生のボタン
+//       floatingActionButton:
+//           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+//         FloatingActionButton(
+//             onPressed: _forward, child: const Icon(Icons.arrow_forward)),
+//         FloatingActionButton(onPressed: _stop, child: const Icon(Icons.pause)),
+//         FloatingActionButton(
+//             onPressed: _reverse, child: const Icon(Icons.arrow_back)),
+//       ]),
+//     );
+//   }
+// }
 
 // Tween/Animation
 // class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
