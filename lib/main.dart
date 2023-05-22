c70b5a6f5ec03be0c1954435613a284c74606c49
tsubasa-sbym <gauvm59mx@yahoo.co.jp>
2023-05-22 18:24:38 +0900

    feat: 画像取得＆表示実装（カメラとギャラリーで写真(画像)を取得して画面に表示）

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'test_page1.dart';
-import 'test_page2.dart';
-import 'test_page3.dart';
-
-// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-// import 'package:url_launcher/url_launcher_string.dart';
-// import 'dart:math';
-// import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,444 +49,54 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
 class _MyHomePageState extends State<MyHomePage> {
-  late PageController _pageController;
-  int _selectedIndex = 0;
+  XFile? _image;
+  final imagePicker = ImagePicker();
 
-  // ページの配列
-  final _pages = [
-    TestPage1(),
-    TestPage2(),
-    TestPage3(),
-  ];
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(initialPage: _selectedIndex);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _pageController.dispose();
-  }
-
-  void _onPageChanged(int index) {
+  // カメラから写真を取得するメソッド
+  Future getImageFromCamera() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.camera);
     setState(() {
-      _selectedIndex = index;
+      if (pickedFile != null) {
+        _image = XFile(pickedFile.path);
+      }
+    });
+  }
+
+  // ギャラリーから写真を取得するメソッド
+  Future getImageFromGarally() async {
+    final pickedFile = await imagePicker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedFile != null) {
+        _image = XFile(pickedFile.path);
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: PageView(
-            controller: _pageController,
-            onPageChanged: _onPageChanged,
-            children: _pages));
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+            // 取得した写真を表示(ない場合はメッセージ)
+            child: _image == null
+                ? Text(
+                    '写真を選択してください',
+                    style: Theme.of(context).textTheme.headline4,
+                  )
+                : Image.file(File(_image!.path))),
+        floatingActionButton:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          // カメラから取得するボタン
+          FloatingActionButton(
+              onPressed: getImageFromCamera,
+              child: const Icon(Icons.photo_camera)),
+          // ギャラリーから取得するボタン
+          FloatingActionButton(
+              onPressed: getImageFromGarally,
+              child: const Icon(Icons.photo_album))
+        ]));
   }
 }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(body: TestPage1());
-//   }
-// }
-
-// AnimationBuilder
-// class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-//   late AnimationController _animationController;
-//   late Animation _animation;
-
-//   // 再生
-//   _forward() async {
-//     setState(() {
-//       _animationController.forward();
-//     });
-//   }
-
-//   // 停止
-//   _stop() async {
-//     setState(() {
-//       _animationController.stop();
-//     });
-//   }
-
-//   // 逆再生
-//   _reverse() async {
-//     setState(() {
-//       _animationController.reverse();
-//     });
-//   }
-
-//   // 生成
-//   @override
-//   void initState() {
-//     super.initState();
-//     _animationController =
-//         AnimationController(vsync: this, duration: const Duration(seconds: 1));
-//     _animation = _animationController.drive(Tween(begin: 0.0, end: 2.0 * pi));
-//   }
-
-//   // 破棄
-//   @override
-//   void dispose() {
-//     _animationController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: AnimatedBuilder(
-//           animation: _animation,
-//           builder: (context, _) {
-//             return Transform.rotate(
-//                 angle: _animation.value,
-//                 child: const Icon(Icons.cached, size: 100));
-//           },
-//         ),
-//       ),
-//       // 再生、停止、逆再生のボタン
-//       floatingActionButton:
-//           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-//         FloatingActionButton(
-//             onPressed: _forward, child: const Icon(Icons.arrow_forward)),
-//         FloatingActionButton(onPressed: _stop, child: const Icon(Icons.pause)),
-//         FloatingActionButton(
-//             onPressed: _reverse, child: const Icon(Icons.arrow_back)),
-//       ]),
-//     );
-//   }
-// }
-
-// Tween/Animation
-// class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-//   late AnimationController _animationController;
-//   late Animation<double> _animationDouble;
-//   final Tween<double> _tweenDouble = Tween(begin: 0.0, end: 200.0);
-//   late Animation<Color?> _animationColor;
-//   final ColorTween _tweenColor =
-//       ColorTween(begin: Colors.green, end: Colors.blue);
-
-//   // 再生
-//   _forward() async {
-//     setState(() {
-//       _animationController.forward();
-//     });
-//   }
-
-//   // 停止
-//   _stop() async {
-//     setState(() {
-//       _animationController.stop();
-//     });
-//   }
-
-//   // 逆再生
-//   _reverse() async {
-//     setState(() {
-//       _animationController.reverse();
-//     });
-//   }
-
-//   // 生成
-//   @override
-//   void initState() {
-//     super.initState();
-//     _animationController =
-//         AnimationController(vsync: this, duration: const Duration(seconds: 3));
-//     // TweenとAnimationControllerからAnimationを作る(サイズ)
-//     _animationDouble = _tweenDouble.animate(_animationController);
-//     _animationDouble.addListener(() {
-//       setState(() {});
-//     });
-//     // TweenとAnimationControllerからAnimationを作る(色)
-//     _animationColor = _tweenColor.animate(_animationController);
-//     _animationColor.addListener(() {
-//       setState(() {});
-//     });
-//   }
-
-//   // 破棄
-//   @override
-//   void dispose() {
-//     _animationController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text("AnimationController:${_animationController.value}"),
-//             Text("AnimationDouble:${_animationDouble.value}"),
-//             Text("AnimationColor:${_animationColor.value}"),
-//             SizeTransition(
-//               sizeFactor: _animationController,
-//               child: Center(
-//                   child: SizedBox(
-//                       width: _animationDouble.value,
-//                       height: _animationDouble.value,
-//                       child: Container(color: _animationColor.value))),
-//             ),
-//           ],
-//         ),
-//       ),
-//       // 再生、停止、逆再生のボタン
-//       floatingActionButton:
-//           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-//         FloatingActionButton(
-//             onPressed: _forward, child: const Icon(Icons.arrow_forward)),
-//         FloatingActionButton(onPressed: _stop, child: const Icon(Icons.pause)),
-//         FloatingActionButton(
-//             onPressed: _reverse, child: const Icon(Icons.arrow_back)),
-//       ]),
-//     );
-//   }
-// }
-
-//AnimationControllerを使ったTransition系Widgetアニメーション
-// class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-//   late AnimationController _animationController;
-
-//   // 再生
-//   _forward() async {
-//     setState(() {
-//       _animationController.forward();
-//     });
-//   }
-
-//   //停止
-//   _stop() async {
-//     setState(() {
-//       _animationController.stop();
-//     });
-//   }
-
-//   // 逆再生
-//   _reverse() async {
-//     setState(() {
-//       _animationController.reverse();
-//     });
-//   }
-
-//   // 生成
-//   @override
-//   void initState() {
-//     super.initState();
-//     _animationController =
-//         AnimationController(vsync: this, duration: const Duration(seconds: 3));
-//   }
-
-//   // 破棄
-//   @override
-//   void dispose() {
-//     _animationController.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             SizeTransition(
-//               sizeFactor: _animationController, // AnimationControllerを設定
-//               child: Center(
-//                   child: SizedBox(
-//                       width: 50,
-//                       height: 50,
-//                       child: Container(color: Colors.green))),
-//             ),
-//           ],
-//         ),
-//       ),
-//       // 再生、停止、逆再生のボタン
-//       floatingActionButton:
-//           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-//         FloatingActionButton(
-//             onPressed: _forward, child: const Icon(Icons.arrow_forward)),
-//         FloatingActionButton(onPressed: _stop, child: const Icon(Icons.pause)),
-//         FloatingActionButton(
-//             onPressed: _reverse, child: const Icon(Icons.arrow_back)),
-//       ]),
-//     );
-//   }
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-  // bool _flag = false;
-  // _click() async {
-  //   setState(() {
-  //     _flag = !_flag;
-  //   });
-  // }
-  //AnimatedOpacity/AnimatedSize/AnimatedAlign
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text(widget.title),
-  //     ),
-  //     body: Center(
-  //       child: Column(
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: <Widget>[
-  //           AnimatedOpacity(
-  //               opacity: _flag ? 0.1 : 1.0,
-  //               duration: const Duration(seconds: 3),
-  //               child: Text(
-  //                 "消える文字",
-  //                 style: Theme.of(context).textTheme.headline4,
-  //               )),
-  //           AnimatedSize(
-  //               duration: const Duration(seconds: 3),
-  //               child: SizedBox(
-  //                   width: _flag ? 50 : 200,
-  //                   height: _flag ? 50 : 200,
-  //                   child: Container(color: Colors.purple))),
-  //           AnimatedAlign(
-  //               duration: const Duration(seconds: 3),
-  //               alignment: _flag ? Alignment.topLeft : Alignment.bottomRight,
-  //               child: SizedBox(
-  //                   width: 50,
-  //                   height: 50,
-  //                   child: Container(color: Colors.green)))
-  //         ],
-  //       ),
-  //     ),
-  //     floatingActionButton:
-  //       FloatingActionButton(onPressed: _click, child: const Icon(Icons.add)),
-  //   );
-  // }
-
-  //AnimatedContainer/AnimatedSwitcher
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     appBar: AppBar(
-  //       title: Text(widget.title),
-  //     ),
-  //     body: Center(
-  //       child: Column(
-  //         mainAxisAlignment: MainAxisAlignment.center,
-  //         children: <Widget>[
-  //           AnimatedContainer(
-  //               duration: const Duration(seconds: 3),
-  //               width: _flag ? 100 : 50,
-  //               height: _flag ? 50 : 100,
-  //               padding: _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
-  //               margin: _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
-  //               transform: _flag ? Matrix4.skewX(0.0) : Matrix4.skewX(0.3),
-  //               color: _flag ? Colors.blue : Colors.grey),
-  //           AnimatedSwitcher(
-  //               duration: const Duration(seconds: 3),
-  //               child: _flag
-  //                   ? const Text("なにもない")
-  //                   : const Icon(Icons.favorite, color: Colors.pink))
-  //         ],
-  //       ),
-  //     ),
-  //     floatingActionButton:
-  //       FloatingActionButton(onPressed: _click, child: const Icon(Icons.add)),
-  //   );
-  // }
-//   int _counter = 0;
-//   bool _setValue = false;
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Row(children: const [
-//          Icon(Icons.create),
-//          Text("タイトル"),
-//        ]),
-//       ),
-//       body: Column(children: [
-//          const Center(child: Icon(FontAwesomeIcons.gift, color: Colors.teal)),
-//          const Text("HelloWorld"),
-//          const Text("ハローワールド"),
-//          Text(
-//              '$_counter',
-//              style: Theme.of(context).textTheme.headlineMedium,
-//           ),
-//           if (_counter % 2 == 0)
-//              const Text('偶数です', style: TextStyle(fontSize: 20, color: Colors.red)),
-//          TextButton(
-//            onPressed: () => {print("ボタンが押されたよ")},
-//            child: const Text("テキストボタン"),
-//          ),
-//          Row(
-//              mainAxisAlignment: MainAxisAlignment.spaceAround,
-//              children: const [
-//                Icon(
-//                  Icons.favorite,
-//                  color: Colors.pink,
-//                  size: 24.0,
-//                ),
-//                Icon(
-//                  Icons.audiotrack,
-//                  color: Colors.green,
-//                  size: 30.0,
-//                ),
-//                Icon(
-//                  Icons.beach_access,
-//                  color: Colors.blue,
-//                  size: 36.0,
-//                ),
-//              ]),
-//              IconButton(
-//               icon: const Icon(Icons.search),
-//               onPressed: () async {
-//                   String url = Uri.encodeFull(
-//                       "https://www.google.co.jp/search?q=flutter");
-//                   await canLaunchUrlString(url);
-//                   await launchUrlString(url);
-              
-//               }),
-//       ]),
-//       floatingActionButton: FloatingActionButton(
-//          onPressed: _incrementCounter, tooltip: 'Increment', child: const Icon(Icons.add)),
-//       drawer: const Drawer(child: Center(child: Text("Drawer"))),
-//       endDrawer: Drawer(
-//         child: ListView(children: <Widget>[
-//           SwitchListTile(
-//           title:const  Text('記念日'),
-//           value: _setValue,
-//           //スイッチが変更された時のイベントを定義する
-//           onChanged: (bool value) {
-//             setState(() {
-//               _setValue = value;
-//             });
-//             // 変更時のイベントが続く
-//           },
-//           secondary: const Icon(FontAwesomeIcons.gift, color: Colors.teal),
-//           ),
-//         ]),
-//       ),
-//     );
-//   }
-// }
